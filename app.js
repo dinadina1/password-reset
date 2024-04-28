e1ef610dee1089992f501d6b4b182085f591eb01
dinadina1 <dinad9355@gmail.com>
2024-04-28 12:40:31 +0530

    password-reset

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 const whitelist = ['http://localhost:3500']
 const corsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) == -1 || origin) {
+    if (whitelist.indexOf(origin) !== -1 || origin) {
         callback(null, true)
       } else {
         callback(new Error('Not allowed by CORS'))

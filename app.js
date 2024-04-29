77fcc2d5e0629639c9ad8f4a0001beed46d639e6
dinadina1 <dinad9355@gmail.com>
2024-04-29 10:14:11 +0530

    Cors updated

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // require express
-const express = require('express');
+const express = require("express");
 
 // create an instance of express
 const app = express();
@@ -7,8 +7,15 @@ const app = express();
 // require cors
 // const cors = require('cors');
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 // parse the body of the request
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // create whitelist for cors
@@ -20,14 +27,10 @@ app.use(express.json());
 // app.use(cors(corsOptions));
 
 // require cookie-parser
-app.use(require('cookie-parser')());
+app.use(require("cookie-parser")());
 
 // require user routes
-app.use('/', require('./routes/user'));
+app.use("/", require("./routes/user"));
 
 // export app to be used in app.js
 module.exports = app;
-
-
-
-

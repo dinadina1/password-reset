7fd9df429fb61a92ed3cdb9d8520c8af9ae7b0fe
dinadina1 <dinad9355@gmail.com>
2024-04-29 21:43:21 +0530

    Cors updated

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 // const cors = require('cors');
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
